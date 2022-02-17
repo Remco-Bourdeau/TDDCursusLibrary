1f38281118d24d674f680e38a59a3b3640dec0ec
REMCO BOURDEAU <REMCO.BOURDEAU@vdabcampus.be>
2022-02-17 09:45:37 +0100

    oefening palindroom

---
+++ Woord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCursusLibrary
+{
+    public class Woord
+    {
+        private readonly string woord;
+        public Woord(string woord)
+        {
+            this.woord = woord;
+        }
+        public bool IsPalindroom
+        {
+            get
+            {
+                if (woord.SequenceEqual(woord.Reverse()))
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+
+    }
+}
